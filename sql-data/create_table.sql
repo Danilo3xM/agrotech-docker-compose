5e7806121496a0a0e89d99c4f83b257636087b20
Danilo Martins Maximiano Mancini <danilo.3xm@gmail.com>
2021-04-11 21:53:10 -0300

    Ajuste no create.sql

--- sql-data/create_table.sql
+++ sql-data/create_table.sql
@@ -1,8 +1,18 @@
 CREATE TABLE IF NOT EXISTS DRONES(
-  id INT NOT NULL,
+  id SERIAL PRIMARY KEY,
   latitude varchar(15) NOT NULL,
   longitude varchar(15) NOT NULL,
-  temperatura INT NOT NULL,
-  umidade INT NOT NULL,
-  PRIMARY KEY (product_id)
+  temperatura integer NOT NULL,
+  umidade integer NOT NULL
 );
+
+INSERT INTO DRONES(latitude, longitude, temperatura, umidade) VALUES
+  ('-23°32’51”S', '100°01’10”W', 30, 90);
+INSERT INTO DRONES(latitude, longitude, temperatura, umidade) VALUES
+  ('-23°32’51”S', '100°01’10”W', 30, 90);
+INSERT INTO DRONES(latitude, longitude, temperatura, umidade) VALUES
+  ('-23°32’51”S', '100°01’10”W', 30, 90);
+INSERT INTO DRONES(latitude, longitude, temperatura, umidade) VALUES
+  ('-23°32’51”S', '100°01’10”W', -1, 90);
+INSERT INTO DRONES(latitude, longitude, temperatura, umidade) VALUES
+  ('-23°32’51”S', '100°01’10”W', 2, 15);
